f0eb4c33b69c215401fde20d7af82835cd3e8488
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-09-06 18:37:21 +0200

    Delete button and route

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,6 +7,18 @@
     <div>Slug: {{$post->slug}}</div>
     <div>Creato il: {{$post->created_at}}</div>
     <div>Ultima modifica avvenuta il: {{$post->updated_at}}</div>
-    <a href="{{route('admin.posts.edit', ['post'=>$post->id])}}" class="btn btn-primary">Modifica articolo</a>
+    {{-- Inserisco il pulsante per modificare l'articolo --}}
+    <div>
+        <a href="{{route('admin.posts.edit', ['post'=>$post->id])}}" class="btn btn-primary">Modifica articolo</a>
+    </div>
+    {{-- Inserisco il pulsante per eliminare l'articolo--}}
+    <div>
+        <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="post">
+            @csrf
+            @method('DELETE')
+
+            <input type="submit" value="Cancella" onClick="return confirm('Sei sicuro di voler cancellare?');" class="btn btn-danger">
+        </form>
+    </div>
 
 @endsection
